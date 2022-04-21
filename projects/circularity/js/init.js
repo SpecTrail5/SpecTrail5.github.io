6456f160806b2460b74b746826c45f645439f12a
SpecTrail5 <elijahiwatson2003@gmail.com>
2022-04-21 00:55:32 +0000

    circles, that's right circles

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,27 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,11 +53,26 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+            physikz.updatePosition(circles[5]);
+            physikz.updatePosition(circles[6]);
+            physikz.updatePosition(circles[7]);
+            physikz.updatePosition(circles[8]);
 
-            
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            game.checkCirclePosition(circles[0])
+            game.checkCirclePosition(circles[1])
+            game.checkCirclePosition(circles[2])
+            game.checkCirclePosition(circles[3])
+            game.checkCirclePosition(circles[4])
+            game.checkCirclePosition(circles[5])
+            game.checkCirclePosition(circles[6])
+            game.checkCirclePosition(circles[7])
+            game.checkCirclePosition(circles[8])
             // TODO 9 : Iterate over the array
            
             
