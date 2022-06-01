630c7841fc8f855a18ec425006b32b07d4b2b83f
SpecTrail5 <elijahiwatson2003@gmail.com>
2022-06-01 18:32:43 +0000

    adding ASD projects

---
+++ asd-projects/image-filtering/index.js
@@ -0,0 +1,34 @@
+// This is a small program. There are only two sections. This first section is what runs
+// as soon as the page loads and is where you should call your functions.
+$(document).ready(function(){
+    const $display = $('#display');
+
+    // Multiple TODOs: Call your apply function(s) here
+
+
+
+
+
+    render($display, image);
+});
+
+/////////////////////////////////////////////////////////
+// "apply" and "filter" functions should go below here //
+/////////////////////////////////////////////////////////
+
+// TODO 1, 2 & 4: Create the applyFilter function here
+
+
+// TODO 7: Create the applyFilterNoBackground function
+
+
+// TODO 5: Create the keepInBounds function
+
+
+// TODO 3: Create reddify function
+
+
+// TODO 6: Create more filter functions
+
+
+// CHALLENGE code goes below here
