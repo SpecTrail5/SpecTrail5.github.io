664f801ec9fa6547abb33c170787e45ece23b2b5
IoT <IoT@Supers-Mac-mini-3.local>
2022-09-29 18:58:17 -0500

    saving are-you-being-served

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,28 @@
 // requestServer.js file
+const http = require("http");
+const request = require("request");
+var port = 9480;
+
+// example http server creation
+http.createServer(function (req, res) {
+    request("https://github.com/SpecTrail5/SpecTrail5.github.io", function (error, response, body) {
+  if (!body || !response || (error === null && response.statusCode !== 200)) {
+    res.end("bad URL\n");
+    return;
+  }
+
+  if(response.statusCode === 200 && !error === true){
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.write('Yes, come on in,')
+    res.end(' but please leave.')
+  }else{
+    res.writeHead(response.statusCode, {'content-type': 'text/plain'});
+    res.write(error.toString())
+    res.end(', oh well')
+  }
+
+});
+
+}).listen(port);
+
+
