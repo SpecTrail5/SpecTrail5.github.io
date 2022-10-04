1e5f5790500752fc4ec395a8a4a08e6d3a6c23ca
IoT <IoT@operationsmini6.lan>
2022-10-04 18:32:35 -0500

    saving are-you-being-served

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,28 +1,35 @@
 // requestServer.js file
+
+var args = process.argv.slice(2);
 const http = require("http");
 const request = require("request");
-var port = 9480;
+var port = 5840;
 
 // example http server creation
-http.createServer(function (req, res) {
-    request("https://github.com/SpecTrail5/SpecTrail5.github.io", function (error, response, body) {
-  if (!body || !response || (error === null && response.statusCode !== 200)) {
-    res.end("bad URL\n");
-    return;
-  }
+http
+  .createServer(function (req, res) {
+    var url = args[0] ? args[0] : "https://spectrail5.github.io/";
+    request(url, function (error, response, body) {
 
-  if(response.statusCode === 200 && !error === true){
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write('Yes, come on in,')
-    res.end(' but please leave.')
-  }else{
-    res.writeHead(response.statusCode, {'content-type': 'text/plain'});
-    res.write(error.toString())
-    res.end(', oh well')
-  }
+      if (
+        !body ||
+        !response ||
+        (error === null && response.statusCode !== 200)
+      ) {
+        res.end("bad URL\n");
+        return;
+      }
 
-});
-
-}).listen(port);
-
-
+      if (response.statusCode === 200 && !error === true) {
+        res.writeHead(200, { "content-type": url });
+        res.write("Yes, come on in,");
+        res.write(" but please leave. ");
+        res.end(url);
+      } else {
+        res.writeHead(response.statusCode, { "content-type": "text/plain" });
+        res.write(error.toString());
+        res.end(", oh well");
+      }
+    });
+  })
+  .listen(port);
