343436c024586e90eeaf9d97945288ea5839feeb
IoT <IoT@Supers-Mac-mini-3.local>
2022-10-13 17:38:33 -0500

    saving RNG

--- personal-projects/RNG/rng.js
+++ personal-projects/RNG/rng.js
@@ -10,13 +10,24 @@ function runProgram(){
 
   var num = [5]
 
-  var num_of_num = $('#num-number').text()
+  var num_of_num = document.getElementById("num-of-num").value;
 
-  var between_num1 = $('#between-num1')
+  var between_num = {
+    num1: document.getElementById("between-num1").value,
+    num2: document.getElementById("between-num2").value
+  }
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);
+  $(document).on('keyup',handleKeyUp)
 
   function newFrame(){
-    $("#num").text(num)
+    num_of_num = document.getElementById("num-of-num").value
+   
+  }
+
+  function handleKeyUp(event){
+    if (event.which === KEY.space){
+         $("#num").text(num_of_num)
+    }
   }
 
 
