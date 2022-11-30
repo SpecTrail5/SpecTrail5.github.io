7cd6af629aadb9c7157d603b75703c5e4b73a9a1
IoT <IoT@Operations-Mac-mini-6.local>
2022-11-29 18:20:59 -0600

    saving

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,6 +1,14 @@
 const express = require('express'),
 	cors = require('cors');
-	
+
+	var app = express();
+	app.use(cors());
+	app.get('/', function(req, res){
+		res.send('Why do you want to be here.');
+	 });
+	 app.get('/pi', function(req, res){
+		res.send('Nope.');
+	 });
 
 module.exports = app;
 // I have looked through all files
