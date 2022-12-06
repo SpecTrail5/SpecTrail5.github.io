087752b2c824062f80063bae4bd65d4263fe81c2
IoT <IoT@Operations-Mac-mini-6.local>
2022-12-06 17:06:55 -0600

    saving

--- iot-projects/first-slice-of-pi/plugins/internal/pirPlugin.js
+++ iot-projects/first-slice-of-pi/plugins/internal/pirPlugin.js
@@ -1,6 +1,25 @@
-const resources = require('./../../resources/model');
-const Gpio = require('onoff').Gpio;
+const resources = require("./../../resources/model");
+const Gpio = require("onoff").Gpio;
 
 let sensor;
 const device = resources.pi.sensors.pir;
 
+function connectHardware() {
+  sensor = new Gpio(device.gpio, "in", "both");
+
+  sensor.watch(function (err, value) {
+    if (err) exit(err);
+    console.log(value ? "there is someone!" : "not anymore!");
+  });
+}
+
+function start() {
+  connectHardware();
+}
+
+function stop() {
+  sensor.unexport();
+}
+
+exports.start = start;
+exports.stop = stop;
--- iot-projects/first-slice-of-pi/wot-server.js
+++ iot-projects/first-slice-of-pi/wot-server.js
@@ -1,10 +1,12 @@
-const httpServer = require('./servers/http'),
-	resources = require('./resources/model');
+const httpServer = require("./servers/http"),
+  resources = require("./resources/model");
+var pirPlugin = require("./plugins/internal/pirPlugin");
 
 const server = httpServer.listen(resources.pi.port, function () {
-	console.log("Running the Pi on port " + resources.pi.port);
+  console.log("Running the Pi on port " + resources.pi.port);
 });
 
-process.on('SIGINT', function() {
-	process.exit();
+process.on("SIGINT", function () {
+  pirPlugin.stop();
+  process.exit();
 });
