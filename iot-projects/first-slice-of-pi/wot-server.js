41066f76e3fb992447bcfc08c0224f8d53411b8c
IoT <IoT@Operations-Mac-mini-6.local>
2022-12-06 17:41:36 -0600

    saving

--- iot-projects/first-slice-of-pi/wot-server.js
+++ iot-projects/first-slice-of-pi/wot-server.js
@@ -1,6 +1,5 @@
 const httpServer = require("./servers/http"),
   resources = require("./resources/model");
-var pirPlugin = require("./plugins/internal/pirPlugin");
 
 const server = httpServer.listen(resources.pi.port, function () {
   console.log("Running the Pi on port " + resources.pi.port);
@@ -10,3 +9,5 @@ process.on("SIGINT", function () {
   pirPlugin.stop();
   process.exit();
 });
+
+var pirPlugin = require("./plugins/internal/pirPlugin");
