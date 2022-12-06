07fe416df3d9444067e255c25eea49b3d9b33a0f
IoT <IoT@Operations-Mac-mini-6.local>
2022-12-06 16:37:00 -0600

    saving

--- iot-projects/first-slice-of-pi/routes/actuators.js
+++ iot-projects/first-slice-of-pi/routes/actuators.js
@@ -1,17 +1,15 @@
-const express = require('express'),
-	router = express.Router(),
-	resources = require('./../resources/model');
+const express = require("express"),
+  router = express.Router(),
+  resources = require("./../resources/model");
 
-	router.route("/").get(function (req, res, next) {
-		res.send(resources.pi.actuators);
-	  });
-	  router.route("/leds").get(function (req, res, next) {
-		res.send(resources.pi.actuators);
-	  });
-	  router.route("/leds/:id").get(function (req, res, next) {
-		res.send(resources.pi.actuators.leds[req.params.id]);
-	  });
-	  
-	
+router.route("/").get(function (req, res, next) {
+  res.send(resources.pi.actuators);
+});
+router.route("/leds").get(function (req, res, next) {
+  res.send(resources.pi.actuators);
+});
+router.route("/leds/:id").get(function (req, res, next) {
+  res.send(resources.pi.actuators.leds[req.params.id]);
+});
 
 module.exports = router;
--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,28 +1,18 @@
-var sensorRoutes = require('./../routes/sensors');
-var actuatorRoutes = require('./../routes/actuators');
+var sensorRoutes = require("./../routes/sensors");
+var actuatorRoutes = require("./../routes/actuators");
 const express = require("express"),
   cors = require("cors");
 
 var app = express();
 app.use(cors());
-app.use('/pi/sensors', sensorRoutes);
+app.use("/pi/sensors", sensorRoutes);
 app.get("/", function (req, res) {
   res.send("Why do you want to be here.");
 });
 app.get("/pi", function (req, res) {
   res.send("Nope.");
 });
-
-app.use(cors());
-app.use('/pi/actuators', actuatorRoutes);
-app.get("/", function (req, res) {
-  res.send("Nope, you really should leave.");
-});
-app.get("/pi", function (req, res) {
-  res.send("HA.");
-});
-
-
+app.use("/pi/actuators", actuatorRoutes);
 
 module.exports = app;
 // I have looked through all files
