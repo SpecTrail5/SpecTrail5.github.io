436c5a68e8334406e6695d57fa04993262f64ea6
IoT <IoT@Operations-Mac-mini-6.local>
2022-12-06 18:55:35 -0600

    saving

--- iot-projects/first-slice-of-pi/plugins/internal/dhtPlugin.js
+++ iot-projects/first-slice-of-pi/plugins/internal/dhtPlugin.js
@@ -1,7 +1,39 @@
-const resources = require('./../../resources/model');
-const sensorDriver = require('node-dht-sensor');
+const resources = require("./../../resources/model");
+const sensorDriver = require("node-dht-sensor");
 
 let interval, sensor;
 const device = resources.pi.sensors.dht;
-let localParams = {'frequency': 2000};
+let localParams = { frequency: 2000 };
 
+function connectHardware() {
+  sensor = {
+    initialize: function () {
+      sensorDriver.initialize(device.model, device.gpio);
+    },
+    read: function () {
+      var whateverYouWish = sensorDriver.read;
+
+      device.temperature.value = parseFloat(whateverYouWish.temperature);
+      device.humidity.value = parseFloat(whateverYouWish.humidity);
+    },
+  };
+
+  sensor.initialize();
+  sensor.read();
+
+  interval = setInterval(function () {
+    sensor.read();
+  }, localParams.frequency);
+}
+
+function start(params) {
+  localParams = params ? params : localParams;
+  connectHardware();
+}
+
+function stop() {
+  clearInterval(interval);
+}
+
+exports.start = start;
+exports.stop = stop;
--- iot-projects/first-slice-of-pi/wot-server.js
+++ iot-projects/first-slice-of-pi/wot-server.js
@@ -1,15 +1,18 @@
 var pirPlugin = require("./plugins/internal/pirPlugin");
+var dhtPlugin = require("./plugins/internal/dhtPlugin");
+
 const httpServer = require("./servers/http"),
   resources = require("./resources/model");
 
 pirPlugin.start({});
+dhtPlugin.start({ frequency: 2000 });
+
 const server = httpServer.listen(resources.pi.port, function () {
   console.log("Running the Pi on port " + resources.pi.port);
 });
 
 process.on("SIGINT", function () {
   pirPlugin.stop();
+  dhtPlugin.stop();
   process.exit();
 });
-
-
