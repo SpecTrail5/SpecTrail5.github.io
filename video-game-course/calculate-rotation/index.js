f950d58ddfa3f2ee63f78f5076a83af0930701bf
SpecTrail5 <elijahiwatson2003@gmail.com>
2023-03-22 00:11:54 +0000

    quick save **

--- video-game-course/calculate-rotation/index.js
+++ video-game-course/calculate-rotation/index.js
@@ -1,5 +1,5 @@
 // TODO 4: add a param for your game lib last //
-(function(window, opspark) {
+(function(window, opspark, idk) {
   console.log('index.js initialized!');
 
   const
@@ -12,13 +12,17 @@
   stage.addChild(textfield);
 
   // try a different hex color if you want //
-  const ship = assets.makeShip('#4286f4');
+  const ship = assets.makeShip('#C32626');
   
   
   // TODO 5: Center the ship on the stage //
-  
+
+  ship.x = canvas.width/2  
+  ship.y = canvas.height/2  
 
   // TODO 6: Add the ship to the stage //
+
+  stage.addChild(ship)
   
 
   
@@ -34,18 +38,25 @@
      * method takes two points. What do you need to do to translate
      * these values such that they're packed into a point?
      */
+
+    var mouse = {
+      x: stage.mouseX,
+      y: stage.mouseY
+    }
+
+    const degrees = idk.numz.getAngleDegrees(ship, mouse)
     
     
     // TODO 8: Set the ship's rotation property to the degrees //
     
-    
+    ship.rotation = degrees
     
     /*
      * TODO 9: Uncomment the line below to update the textfield  
      * with the current angle degrees. Degrees will be a value 
      * between π and -π, or, 180 and -180.
      */
-    // assets.updateText(textfield, `Degrees: ${degrees.toFixed(3)}°`, canvas);
+     assets.updateText(textfield, `Degrees: ${degrees.toFixed(3)}°`, canvas);
   }
 
   engine
@@ -53,4 +64,4 @@
     .activateTick();
 
 // TODO 3: pass your game lib last with, window.my-game-lib //
-}(window, window.opspark));
+}(window, window.opspark, window.idk));
